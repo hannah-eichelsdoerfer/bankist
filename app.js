4930957d276abf13441cb1543eb6600af700142c
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-08-03 11:13:16 +0200

    Color Change

--- app.js
+++ app.js
@@ -3,26 +3,59 @@
 // Data
 const account1 = {
   owner: 'Hannah Eichelsdoerfer',
-  transactions: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  transactions: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1996,
+
+  transactionsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-07-26T17:01:17.194Z',
+    '2020-07-28T23:36:17.929Z',
+    '2020-08-01T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
+  owner: 'Jonas Schmedtmann',
+  transactions: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 1111,
+
+  transactionsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+}
+
+const account3 = {
   owner: 'Bill Gates',
   transactions: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 1111,
 };
 
-const account3 = {
+const account4 = {
   owner: 'Steve Jobbs',
   transactions: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 2222,
 };
 
-const account4 = {
+const account5 = {
   owner: 'Michelle Obama',
   transactions: [430, 1000, 700, 50, 90],
   interestRate: 1,
@@ -63,7 +96,7 @@ const updateStatements = (acc) => {
 loginBtn.addEventListener("click", (event) => {
   event.preventDefault();
   currentAccount = accounts.find(acc => acc.username === loginUsernameElement.value);
-  if (currentAccount?.pin === Number(loginPINelement.value)) {
+  if (currentAccount?.pin === +(loginPINelement.value)) { // +() === Number()
     welcomeParagraph.textContent = `Welcome back, ${currentAccount.owner.split(" ")}`;
     bankingInterface.classList.remove("hidden");
     loginCredentials.classList.add("hidden");
@@ -131,7 +164,7 @@ const displayTransactions = (transactions, sort = false) => {
     const transactionType = transaction > 0 ? "deposit" : "withdrawl"
     const transactionHTML = `
     <div class="transaction">
-      <span class="transaction-type">${index + 1} ${transactionType}</span>
+      <span class="transaction_type ${transactionType}">${index + 1} ${transactionType}</span>
       <span class="transaction-date">3 days ago</span>
       <span class="transaction-value">${transaction}€</span>
     </div>
@@ -156,7 +189,7 @@ const transferAmountInput = document.querySelector("#transfer-amount-input");
 
 transferBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  const amount = Number(transferAmountInput.value);
+  const amount = +(transferAmountInput.value);
   const receiver = accounts.find(acc => acc.username === transferReceiverInput.value);
   transferAmountInput.value = transferReceiverInput.value = "";
   if (receiver && amount > 0 && currentAccount.balance >= amount && receiver?.username !== currentAccount.username) {
@@ -174,7 +207,7 @@ const inputLoanAmount = document.querySelector("#loan-amount-input")
 
 getLoanBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  const amount = Number(inputLoanAmount.value)
+  const amount = +(inputLoanAmount.value); // +() === Number()
   if (amount > 0 && currentAccount.transactions.some(transaction => transaction >= amount * 0.1)) {
     currentAccount.transactions.push(amount);
     updateStatements(currentAccount);
@@ -191,7 +224,7 @@ const closeBtn = document.querySelector("#close-button");
 
 closeBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  if (inputCloseUsername.value === currentAccount.username && Number(inputClosePIN.value) === currentAccount.pin) {
+  if (inputCloseUsername.value === currentAccount.username && +(inputClosePIN.value) === currentAccount.pin) { // +() === Number()
     const index = accounts.findIndex(acc => acc.username === currentAccount.username)
     accounts.splice(index, 1);
     logoutOrClose();
